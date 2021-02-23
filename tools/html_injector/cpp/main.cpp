fef3869239082e1d334ac628b725f5701334b460
asdi952 <ric.sil.eee@gmail.com>
2021-02-23 13:35:43 +0000

    Starting point 23/2/1995

---
+++ tools/html_injector/cpp/main.cpp
@@ -0,0 +1,13 @@
+#include "aux.h"
+
+#define print_(X) std::cout << X << "\n"
+
+class 
+
+
+
+int main(int argc, char** argv)
+{
+  print_("program start");
+  return 0;
+}
---
+++ tools/html_injector/create_folders.sh
@@ -0,0 +1,17 @@
+mkdir cpp
+mkdir header
+mkdir bin
+
+cd bin
+mkdir normal
+mkdir debug
+
+cd normal
+mkdir depend
+mkdir objects
+mkdir preHeader
+
+cd ../debug
+mkdir depend
+mkdir objects
+mkdir preHeader
